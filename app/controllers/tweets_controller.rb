b3475d75965af2843b788f63e8807a78c3efaf93
Thiago <thiagodsalles@gmail.com>
2022-04-13 20:33:29 -0300

    add pagination on Tweets#index and show by newest ones

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,14 @@
 class TweetsController < ApplicationController
   def index
-    render json: Tweet.all
+    tweets = Paginator.new(
+      query: Tweet.all.by_newest_first,
+      per_page: permited_params[:per_page]
+    ).call
+
+    render json: tweets
+  end
+
+  def permited_params
+    params.permit(:per_page)
   end
 end
--- spec/requests/tweet_request_spec.rb
+++ spec/requests/tweet_request_spec.rb
@@ -2,4 +2,26 @@ require 'rails_helper'
 
 RSpec.describe "Tweets", type: :request do
 
+  describe '#index' do
+    let!(:tweets) { create_list(:tweet, 3) }
+    let(:result) { JSON.parse(response.body) }
+
+    context 'when passing per_page parameters' do
+      it 'return paginated query' do
+        get tweets_path, params: { per_page: 1 }
+
+        expect(result['total_pages']).to eq(3)
+        expect(result['pages'].last['page']).to eq(3)
+      end
+    end
+
+    context 'when no parameters passed' do
+      it 'return all query in one page' do
+        get tweets_path
+
+        expect(result['total_pages']).to eq(1)
+        expect(result['pages'].first['page']).to eq(1)
+      end
+    end
+  end
 end
